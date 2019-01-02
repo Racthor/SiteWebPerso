f272ea2f696f87d6fb28a66a2ca94e1636112951
Sylvain <kauffeisensylvain@gmail.com>
2019-01-02 21:35:38 +0100

    Première page avec vue.js

---
+++ la-boite-a-trucs/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <portfolioJeuxVideo msg="Bienvenue dans la boite à trucs ! Sur une idée originale de Anonimus"/>
+    <h2>Le titre fonctionne heureusement.</h2>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import portfolioJeuxVideo from './components/portfolioJeuxVideo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    portfolioJeuxVideo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ la-boite-a-trucs/src/components/portfolioJeuxVideo.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="header">
+		<h1>{{ titre }}</h1>
+		<p>{{ msg }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'portfolioJeuxVideo',
+  el: '#header',
+  props() {
+  	return {
+    	msg: String
+    }
+  },
+  data() {
+  	return {
+  		titre: 'La boite à trucs !'
+  	}
+  },
+
+}
+</script>
+
+<style type="text/css">
+	
+</style>
