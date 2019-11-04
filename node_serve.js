cc451650210d57decfbb70c81c40b2f8ff8002a9
Sylvain <kauffeisensylvain@gmail.com>
2019-11-04 15:02:13 +0100

    fix node_serve

--- node_serve.js
+++ node_serve.js
@@ -30,12 +30,12 @@ const httpPort = 8080
 http.createServer((req, res) => {
   fs.readFile('./dist/index.html', 'utf-8', (err, content) => {
     if (err) {
+      response.writeHead(404);
+      respone.write('Whoops! File not found!');
       console.log('We cannot open "./dist/index.html" file.')
     }
 
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    })
+    res.writeHead(200)
 
     res.end(content)
   })
